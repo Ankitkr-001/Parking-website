f66918ef59ff3aadbe881bb0902102d31f1dfc90
Ankitkr-001 <antitgolu123@gmail.com>
2024-04-21 19:43:16 +0530

    form added

--- src/Components/Feedback/Feedback.jsx
+++ src/Components/Feedback/Feedback.jsx
@@ -1,30 +1,106 @@
-import React from 'react'
-import './Feedback.css'
+import React from "react";
+import "./Feedback.css";
+
+import { useState } from "react";
 
 const Feedback = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleRatingChange = (e) => {
+    setRating(parseInt(e.target.value));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission with the rating value
+    console.log("Submitted rating:", rating);
+  };
+
   return (
-    <div className='feedback'>
-      <form className='feedback-form'>
-        <label htmlFor="">Enter Vechile Number:</label>
-        <input type="text"  required/>
-        <br />
-        <br />
-        <label htmlFor="">Select vehicle type:</label>
-        <select name="" id="" required>
-          <option value="2-wheeler">2 Wheeler</option>
-          <option value="4-wheeler">4 Wheeler</option>
-        </select>
-        <br /><br />
-        <label htmlFor="">Your Name:</label>
+    <div className="feedback">
+      <form className="rating-form" onSubmit={handleSubmit}>
+        <label htmlFor="">Enter Your Name:</label>
         <input type="text" required />
+        <br />
+        <br />
+        <label htmlFor="">Enter Your Email:</label>
+        <input type="email" required />
+        <br />
+        <br />
+        <label htmlFor="">What would you like to see improved</label>
+        <input type="text" required />
+        <br />
+        <br />
+        
+        <label className="rating-label">
+          Rate us:
+          <br />
+          <input
+            className="rating-input"
+            type="radio"
+            name="rating"
+            value="1"
+            checked={rating === 1}
+            onChange={handleRatingChange}
+          />
+          1
+          &nbsp;
+          &nbsp;
+          <input
+            className="rating-input"
+            type="radio"
+            name="rating"
+            value="2"
+            checked={rating === 2}
+            onChange={handleRatingChange}
+          />
+          
+          2
+          &nbsp;
+          &nbsp;
+          <input
+            className="rating-input"
+            type="radio"
+            name="rating"
+            value="3"
+            checked={rating === 3}
+            onChange={handleRatingChange}
+          />
+         
+          3
+          &nbsp;
+          &nbsp;
+          <input
+            className="rating-input"
+            type="radio"
+            name="rating"
+            value="4"
+            checked={rating === 4}
+            onChange={handleRatingChange}
+          />
+         
+          4
+          &nbsp;
+          &nbsp;
+          <input
+            className="rating-input"
+            type="radio"
+            name="rating"
+            value="5"
+            checked={rating === 5}
+            onChange={handleRatingChange}
+          />
+          5
+          
+        </label>
 
-        <button className="vechile-park-btn" type="submit"  >Submit</button>
-        {/* <Link to={'/unparking'}><button className="vechile-park-btn">Unparking</button></Link>
-        <Link to={'/'}><button className="vechile-park-btn">Go Back</button></Link> */}
-
+        
+        <br />
+        <button className="vechile-park-btn" type="submit">Submit</button>
+        <br />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Feedback
+export default Feedback;
